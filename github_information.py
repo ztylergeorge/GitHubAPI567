62c75d2f87afdf198826bff38551e85ab095bad3
ztylergeorge <ztylergeorge@gmail.com>
2020-09-29 00:38:10 -0400

    Initial push of main file and test file

---
+++ github_information.py
@@ -0,0 +1,47 @@
+"""
+Author: Zach George
+Read in a github username
+Output the repository names with the number of commits 
+"""
+
+import requests 
+import json
+from typing import Dict
+
+def github_repo_info(user_ID: str) -> Dict[str, int]:
+
+    """ Read in github username and output the repositories with number of commits """
+
+    HTTPError: Exception = requests.HTTPError
+
+    if not isinstance(user_ID, str):
+        raise ValueError(f"{user_ID} is not a string.")
+
+    #create repository dictionary and user repository string
+    repos_dict: Dict[str: int] = dict() #key: repos value: commit
+    user_repos = "https://api.github.com/users/" + user_ID + "/repos"
+    #attempt to contact website 
+    repos_response = requests.get(user_repos)
+    if repos_response.status_code != 200:
+        raise HTTPError(f"{user_repos} could not be reached.")
+    else:
+        repos_json_info = repos_response.json()
+
+        #take in the name of the repository
+        for i in range(0, len(repos_json_info), 2):
+            repos_dict[repos_json_info[i]["name"]] = 0
+
+        #determine number of commits per repository
+        for repos in repos_dict.keys():
+            commits = "https://api.github.com/repos/" + user_ID + "/" + repos + "/commits"
+            commits_response = requests.get(commits)
+            if commits_response.status_code != 200:
+                raise HTTPError(f"{commits} could not be reached.")
+            else:
+                commits_json_info = commits_response.json()
+                repos_dict[repos] = len(commits_json_info)
+
+    return repos_dict
+    
+    #output results 
+    #print(repos_dict)
---
+++ test_github_information.py
@@ -0,0 +1,40 @@
+import unittest 
+import requests
+from github_information import github_repo_info
+
+
+class GitHubInformationTest(unittest.TestCase):
+
+
+    """ Test the github_repo_info function """
+
+    def test_github_repo_info_1(self):
+
+        """ Test a non string input into the function and expect an error """
+
+        with self.assertRaises(ValueError):
+            github_repo_info(1)
+
+    def test_github_repo_info_2(self):
+
+        """ Test a fake user_ID into the function and expect an error """
+
+        with self.assertRaises(requests.HTTPError):
+            github_repo_info("this_is_a_fake_id")
+
+    def test_github_repo_info_3(self):
+
+        """ Test the richkempinski user_ID and compare to expected keys """
+
+        self.assertEqual(github_repo_info("ztylergeorge").keys(), ['csp', "hellogitworld", "helloworld", "Mocks", 
+                                                                    "Project1", "richkempinski.github.io", "threads-of-life",
+                                                                    "try_nbdev", "try_nbdev2"])
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False, verbosity=2)
